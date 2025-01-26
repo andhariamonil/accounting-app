3a1b3a482d08ddc39f3741f8d89214a8ca36e7e1
andhariamonil <andhariamonil3@gmail.com>
2025-01-26 19:16:16 +0530

    fixed indentation issue in app.py

--- app.py
+++ app.py
@@ -218,5 +218,5 @@ def account_summary():
 
 if __name__ == '__main__':
     init_db()
-   port = int(os.environ.get("PORT", 10000))  # Default port is 10000 if not set by Render
-   app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Default port is 10000 if not set by Render
+    app.run(host="0.0.0.0", port=port, debug=True)
